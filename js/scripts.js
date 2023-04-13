8238fe6d3ab45bdac28120e7a55b28920c5a641e
Asish Ghosh <asish.ghosh@aboutyou.com>
2023-04-13 15:38:03 +0200

    sidebar section

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+// Sidebar open/close
+
+var sidebarOpen = false;
+var sidebar = document.getElementById("sidebar");
+
+function openSidebar() {
+  if (!sidebarOpen) {
+    sidebar.classList.add("sidebar-responsive");
+    sidebarOpen = true;
+  }
+}
+
+function closeSidebar() {
+  if (sidebarOpen) {
+    sidebar.classList.remove("sidebar-responsive");
+    sidebarOpen = false;
+  }
+}
